e7cfd5af24b52287b9fc8c66bcd27e65a780cf83
Kazım Yaman <tugbanuryaman@gmail.com>
2026-06-08 15:12:26 +0300

    sagagsgash

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -355,7 +355,6 @@ export default function AdminPanel() {
               </div>
 
               {/* 5. ÇOKLU GALERİ RESMİ YÜKLEME ALANI */}
-{/* 5. ÇOKLU GALERİ RESMİ YÜKLEME ALANI */}
               <div className="border-2 border-dashed border-neutral-300 rounded-xl p-6 bg-white">
                 <h3 className="text-sm font-bold text-neutral-800 mb-4 flex items-center gap-2">
                   <svg className="w-5 h-5 text-[#db2777]" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
@@ -406,6 +405,7 @@ export default function AdminPanel() {
                     </div>
                   </div>
                 )}
+              </div>
 
               <button type="submit" disabled={status.type === 'loading'} className="w-full bg-neutral-900 text-white font-bold py-5 rounded-xl disabled:bg-neutral-400 hover:bg-[#db2777] transition shadow-[0_10px_20px_rgba(0,0,0,0.1)] hover:shadow-[0_10px_25px_rgba(219,39,119,0.3)] tracking-widest text-lg">
                 YENİ ÜRÜNÜ YAYINLA
